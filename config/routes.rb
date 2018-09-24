1e73b84af3ab271d580608739b59efe0b6b83fe2
pasqu001 <kaseypasqualini@yahoo.com>
2018-09-23 23:29:18 -0400

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :posts do 
+  resources :posts do
     resources :comments
   end
 
   root "posts#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/about', to: 'pages#about'
 end
